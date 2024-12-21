9a8cb8b7e640aa2d9b7d9a7b6a08f195770630e5
aabubakar17 <abubakarabdihakim77@gmail.com>
2024-12-21 16:52:59 +0000

    Initial project setup with Spring Boot

---
+++ Zaktrak-backend/src/main/java/com/ZakTrak/ZakTrakApplication.java
@@ -0,0 +1,13 @@
+package com.ZakTrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakTrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakTrakApplication.class, args);
+	}
+
+}
---
+++ Zaktrak-backend/src/test/java/com/ZakTrak/ZakTrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZakTrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZakTrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
